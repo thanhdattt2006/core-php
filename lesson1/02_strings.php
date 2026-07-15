11cc72c16f8558e52543bdd05891d320553117ca
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-07-15 21:57:54 +0700

    review: strings, single & double quotes

---
+++ lesson1/02_strings.php
@@ -0,0 +1,14 @@
+<?php
+echo "--- 2. STRINGS: SINGLE VS DOUBLE QUOTES ---\n";
+
+$course = "ADSE at Aptech";
+
+// Single Quotes: Output content literally without parsing variables or escape sequences. More lightweight.
+$str1 = 'You are studying $course\n'; 
+
+// Double Quotes: Parse variables and escape sequences (e.g., \n for a new line on CLI).
+$str2 = "You are studying $course\n"; 
+
+echo "Using single quotes: " . $str1 . "\n"; 
+echo "Using double quotes: " . $str2;       
+?>
