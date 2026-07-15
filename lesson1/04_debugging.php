d3ac1254f6eed46dbc236149e557ac69034bd65c
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-07-15 22:06:13 +0700

    review: terminal debugging

---
+++ lesson1/04_debugging.php
@@ -0,0 +1,11 @@
+<?php
+echo "--- 4. TERMINAL DEBUGGING ---\n";
+
+$team = ['Toại', 'Thại', 'Cường', 'Huy'];
+
+echo "[print_r] Displays a human-readable overview of an array:\n";
+print_r($team);
+
+echo "\n[var_dump] Displays detailed information including data types and lengths:\n";
+var_dump($team);
+?>
